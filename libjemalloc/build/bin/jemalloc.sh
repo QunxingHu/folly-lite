8fc7a0b80417bcfb94a841139434597cb321d207
QunxingHu <1367811357@qq.com>
2017-02-13 17:59:34 +0800

    Update cmake rules.

--- libjemalloc/build/bin/jemalloc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-prefix=/home/hqx/build/folly-lite/jemalloc
-exec_prefix=/home/hqx/build/folly-lite/jemalloc
-libdir=${exec_prefix}/lib
-
-LD_PRELOAD=${libdir}/libjemalloc.so.2
-export LD_PRELOAD
-exec "$@"
